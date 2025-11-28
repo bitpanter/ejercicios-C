26e68b733c352987fe50743bb1f18c43e6f06c57
Panter <panternakamoto@gmail.com>
2025-11-27 22:22:20 -0300

    limpieza

--- function.c
+++
@@ -1,4 +0,0 @@
-// Definicion de la funcion suma.
-int suma (int a, int b){
-    return a+b;
-}
--- function.h
+++
@@ -1,7 +0,0 @@
-#ifndef FUNCTION_H
-#define FUNCTION_H
-
-// Prototipo o declaracion de la funcion suma.
-int suma(int a, int b);
-
-#endif
